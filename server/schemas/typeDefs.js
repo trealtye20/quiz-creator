88305dc7f1af6b73e7b505466b430bbd049161d6
KatieG <katieshake27@gmail.com>
2022-01-21 14:47:36 -0500

    merging tyrese's changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
     answer: String
   }
   type Mutation {
-    addQuiz(title: String!, questions: Input_Question!, alottedTime: Int!): Quiz
+    addQuiz(title: String!, questions: Input_Question!, allottedTime: Int!): Quiz
     addUser(userName: String!, email: String!, password: String!, quizzes: [ID]): User
   }
 `;
