b2e828a642c7db45b2d28d112a04c57482ea2d0e
KatieG <katieshake27@gmail.com>
2022-01-21 13:16:12 -0500

    added quiz query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,30 +1,28 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
   type User {
     _id: ID
     userName: String
     email: String
     password: String
     quizzes: [Quiz]
-  
   }
   type Quiz {
     title: String
     questions: [Question]
     highScore: Int
-    alottedTime: Int
+    allottedTime: Int
   }
-type Question {
-  q: String
-  options: [String]
-  answer: String
-}
-type Query {
-  user (_id: ID): User
-}
-  
+  type Question {
+    q: String
+    options: [String]
+    answer: String
+  }
+  type Query {
+    user(_id: ID): User
+    quiz(_id: ID): Quiz
+  }
 `;
 
 module.exports = typeDefs;
