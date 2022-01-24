43d8e3018a1df40ac0bd4595d9ed743ab7f28364
Chris <christangarife95@gmail.com>
2022-01-24 10:49:57 -0500

    reformatted form to include username, removed check out, and added log sign up and spaced the buttons apart

--- client/src/components/LoginSignup.js
+++ client/src/components/LoginSignup.js
@@ -1,50 +1,44 @@
-import React from 'react';
-import '../styles/LoginSignup.css'
-import {Button, Form, Card, Container, Row} from 'react-bootstrap';
+import React from "react";
+import "../styles/LoginSignup.css";
+import { Button, Form, Card, Container, Row } from "react-bootstrap";
 
+function LoginSignup() {
+  return (
+    <Container>
+      <Row>
+        <Card className="col-md-6 mx-auto p-3">
+          <Form id="form-size">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Enter username" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-function LoginSignup () {
-    return (
-      <Container>
-        <Row>
-      <Card className='col-md-6 mx-auto p-3'>
-        <Form id="form-size" > 
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Username</Form.Label>
-    <Form.Control type="username" placeholder="Enter username" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+            <div id="buttons">
+              <Button variant="primary" type="submit">
+                Log-In
+              </Button>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <div id='buttons'>
-  <Button variant="primary" type="submit">
-    Log-In
-  </Button>
-
-  <Button variant="primary" type="submit">
-    Sign-Up
-  </Button>
-</div>
-</Form>
-</Card>
-</Row>
-</Container>
-
-
-    )
+              <Button variant="primary" type="submit">
+                Sign-Up
+              </Button>
+            </div>
+          </Form>
+        </Card>
+      </Row>
+    </Container>
+  );
 }
 
-export default LoginSignup ;
+export default LoginSignup;
