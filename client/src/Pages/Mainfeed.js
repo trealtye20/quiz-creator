3cac2dc60272c9bc1b1c32f0a5361ac2b1336f37
KatieG <katieshake27@gmail.com>
2022-01-24 15:12:19 -0500

    created structure for mainfeed and quizzes

--- client/src/Pages/Mainfeed.js
+++ client/src/Pages/Mainfeed.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React from "react";
+import Header from "../components/Header";
+import Quizzes from "./Quizzes";
+// import LoginSignup from "../components/LoginSignup";
+// import Users from "../components/Users";
+import Footer from "../components/Footer";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_QUIZZES } from "../utils/queries";
 
-
-function Mainfeed () {
-    return (
-        <h1>Place holder</h1>
-    )
-}
-
-
-
-
-
-
-
-
-
-
-
-
+const Mainfeed = () => {
+  const { loading, error, data } = useQuery(GET_ALL_QUIZZES);
+  return (
+    <div>
+      <Header />
+      if (error){<h1>Error Error!</h1>}
+      if (loading){<h1>I'm Loading!</h1>}
+      else
+      {data.map((app) => {
+        return (
+          <Quizzes
+            title={app.title}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+};
 
 export default Mainfeed;
