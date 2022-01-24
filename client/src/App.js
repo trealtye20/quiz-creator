b7149321c02acc6ecbac78b829b467f1e794fb32
trealtye20 <exantustyrese10@gmail.com>
2022-01-24 12:16:55 -0500

    removed user

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import Header from "./components/Header"
 import Footer from './components/Footer';
 import LoginSignup  from './components/LoginSignup';
 import Homepage from './components/Homepage';
+import '../src/styles/App.css';
 
 
 function App() {
   return (
-    <div>
+    <div className="appPage">
       <Header />
       <LoginSignup />
       <Footer />
