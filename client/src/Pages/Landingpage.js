485ac05b39813478c795c9f465b2a38304e50192
Chris <christangarife95@gmail.com>
2022-01-24 12:13:13 -0500

    removed users component from landingpage

--- client/src/Pages/Landingpage.js
+++ client/src/Pages/Landingpage.js
@@ -5,14 +5,37 @@ import Users from "../components/Users";
 import Footer from "../components/Footer";
 
 const Landingpage = () => {
+
+  // const profiles = data?.profiles || [];
+
   return (
     <div>
       <Header />
       <LoginSignup />
-      <Users />
       <Footer />
     </div>
   );
 };
 
 export default Landingpage;
+
+
+
+{/* <main>
+  <div className="flex-row justify-center">
+    <div className="col-12 col-md-10 mb-3 p-3"
+    style={{border: '1px dotted #1a1a1a'}}>
+      <ProfileForm />
+
+    </div>
+
+    <div className="col-12 col-md-10 my-3">
+
+      {Loading ? (<div> Loading... </div>)
+      :
+      {ProfileList profiles={profiles} title= "Here's the current roster.." />)}
+      </div>
+
+    </div>
+  </div>
+</main> */}
