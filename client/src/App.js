6dd6375289c4add86b9b0fe0df966f8c3b816d93
Chris <christangarife95@gmail.com>
2022-01-24 11:38:22 -0500

    created .env and imported apollo client into the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Landingpage from "./Pages/Landingpage";
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
       
       <Landingpage/>
       
     </div>
+    </ ApolloProvider>
   );
 }
 
