89a3fc71d0532a643ed414bb7f08226ca131eae6
KatieG <katieshake27@gmail.com>
2022-01-26 13:15:03 -0500

    worked on timer

--- client/src/QuizApp.js
+++ client/src/QuizApp.js
@@ -1,47 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function QuizApp({props}) {
-  console.log(props)
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+export default function QuizApp({ quiz }) {
+  console.log(`QuizApp: quiz = ${JSON.stringify(quiz)}`);
+  // const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(0);
   const [score, setScore] = useState(0);
   // const [timer, setTimer] = useState(0);
-  // let interval;
+  let interval;
+  let timer = 0;
+  let currentQuestion = 0;
 
-// const startTimer = ()=> {
-//   if (!interval){
-//     interval = setInterval(()=>{
-//       console.log(!interval)
-//       console.log(timer);
-//       if (timer < props.allottedTime)
-//       {setTimer(timer => timer + 1 )}
-//       else {
-//         console.log("else");
-//         handleOptionClick(false)
-//       }
-//     }, 1000)
-//   }
-  
-// }
-// startTimer();
+  useEffect(() => {
+    console.log("useEffect- current question:", currentQuestion);
+    startTimer();
+  }, [currentQuestion]);
 
+  const startTimer = () => {
+    console.log('startTimer');
+    if (!interval) {
+      console.log(interval);
+      interval = setInterval(() => {
+        console.log('interval')
+        console.log(`timer: ${timer}, quiz.allottedTime: ${quiz.allottedTime}`);
+        if (timer < quiz.allottedTime) {
+        // setTimer((timer) => timer + 1 );
+        timer = timer + 1;
+        } else {
+          timer = 0;
+          console.log("else");
+          handleOptionClick(false);
+        }
+      }, 1000);
+    }
+  };
 
   const handleOptionClick = (isCorrect) => {
     if (isCorrect) {
       setScore(score + 20);
     }
+    console.log("current question: ", currentQuestion);
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < props.questions.length) {
-      setCurrentQuestion(nextQuestion);
-      // clearInterval(interval);
+    console.log(`next question: ${nextQuestion}`)
+    if (nextQuestion < quiz.questions.length) {
+      // setCurrentQuestion(nextQuestion);
+      currentQuestion = nextQuestion;
+      clearInterval(interval);
       // startTimer();
     } else {
       setShowScore(true);
     }
   };
-const checkAnswer = (answer, correctAnswer) => {
-return answer == correctAnswer
-}
+  const checkAnswer = (answer, correctAnswer) => {
+    return answer == correctAnswer;
+  };
   return (
     <div className="QuizApp">
       {showScore ? (
@@ -49,11 +60,20 @@ return answer == correctAnswer
       ) : (
         <>
           <div className="questions">
-            <div className="question">{props.questions[currentQuestion].q}</div>
+            <div className="question">{quiz.questions[currentQuestion].q}</div>
           </div>
           <div className="options">
-            {props.questions[currentQuestion].options.map((answerOption) => (
-              <button onClick={() => handleOptionClick(checkAnswer(answerOption, props.questions[currentQuestion].answer))}>
+            {quiz.questions[currentQuestion].options.map((answerOption) => (
+              <button
+                onClick={() =>
+                  handleOptionClick(
+                    checkAnswer(
+                      answerOption,
+                      quiz.questions[currentQuestion].answer
+                    )
+                  )
+                }
+              >
                 {answerOption}
               </button>
             ))}
